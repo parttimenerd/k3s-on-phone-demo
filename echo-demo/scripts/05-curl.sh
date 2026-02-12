48b9d7b38910dc802f297bee13512a24872aa889
Johannes Bechberger <johannes.bechberger@sap.com>
2026-02-12 18:14:25 +0100

    WIP

--- echo-demo/scripts/05-curl.sh
+++ echo-demo/scripts/05-curl.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "curl http://localhost:3000?echo_code=402"
-curl http://localhost:3000?echo_code=402
+echo "curl http://localhost:30080?echo_code=402"
+curl http://localhost:30080?echo_code=402
